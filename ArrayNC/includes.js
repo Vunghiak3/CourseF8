847a830635324907cf520bd2fd3283135fd77ef1
your name <abc@gmail.com>
2022-12-15 17:59:14 +0700

    update

---
+++ ArrayNC/includes.js
@@ -0,0 +1,8 @@
+// includes -- kiểm tra xem kí tự nào đó có trong biến hay không
+var title = "Reponsive web design"
+
+console.log(title.includes('web'));
+console.log(title.includes('Reponsive', 1));
+
+var courses = ['js', 'html', 'css']
+console.log(courses.includes('js', -4));
